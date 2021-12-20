167028ff9a3c1b200b43d51ec4ef7ea30babcd39
jae <wogud1514@naver.com>
2021-12-20 17:40:05 +0900

    리플렉션TEST까지

--- src/main/java/com/high/mvc2/cookie/domain/item/ItemRepository.java
+++ src/main/java/com/high/mvc2/cookie/domain/item/ItemRepository.java
@@ -11,4 +11,24 @@ import java.util.Map;
 public class ItemRepository {
 
     private static final Map<Long,Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(),item);
+        return item;
+    }
+
+    public Item findById(Long itemId){
+        return store.get(itemId);
+    }
+
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() { store.clear(); }
 }
