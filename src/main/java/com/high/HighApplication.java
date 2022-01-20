c4d90a5ac31ea2dd1ad3f3558df0247d3c1ed63a
jae <wogud1514@naver.com>
2022-01-20 23:07:05 +0900

    프록시 패턴 예쩨 V1까지

--- src/main/java/com/high/HighApplication.java
+++ src/main/java/com/high/HighApplication.java
@@ -1,8 +1,11 @@
 package com.high;
 
+import com.high.proxy.app.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppV1Config.class)
 @SpringBootApplication
 public class HighApplication {
 
